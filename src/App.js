e03b4b8847977c750fa82706410221d295feddfc
isabelgmeg <71519606+isabelgmeg@users.noreply.github.com>
2021-10-07 20:58:57 +0200

    finished first block

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import NavBar from "./components/NavBar/NavBar";
+import ExpoMain from "./components/ExpoMain/ExpoMain";
 
 import "./App.scss";
 
@@ -6,6 +7,12 @@ function App() {
   return (
     <div className="App">
       <NavBar />
+      <ExpoMain
+        tittle="La maquina de Magritte"
+        type="Exposición Temporal"
+        date="14.09.21-10.01.22"
+        picture="https://www.renemagritte.org/images/paintings/the-false-mirror.jpg"
+      />
     </div>
   );
 }
