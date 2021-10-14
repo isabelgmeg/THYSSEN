a30b4b84eacef75731dd78f21ad4882499c27fe1
isabelgmeg <71519606+isabelgmeg@users.noreply.github.com>
2021-10-14 12:24:16 +0200

    created subcomponent for visible Wrap

--- src/components/Marquee/Marquee.jsx
+++ src/components/Marquee/Marquee.jsx
@@ -1,33 +1,16 @@
+import MarqueeVisible from "./MarqueeVisible";
 import "./Marquee.scss";
 
 export default function Marquee({ info }) {
-  const populateImages = (images) => {
-    let populatedImages = [...images];
-    let i = 0;
-    while (populatedImages.length < 6) {
-      populatedImages.push(images[i]);
-      i++;
-    }
-    return populatedImages;
-  };
-
   return (
     <div className="displayArtciles">
       {info.map((article, index) => {
         return (
           <div key={index} className="marquee">
-            <div className="marquee__visibleWrap">
-              {populateImages(article.pics).map((singlePic, index) => {
-                return (
-                  <div key={index} className="marquee__visibleWrap__element">
-                    <div className="marquee__image__wrap">
-                      <img src={singlePic} alt={article.tittle}></img>
-                    </div>
-                    <p>{article.tittle}</p>
-                  </div>
-                );
-              })}
-            </div>
+            <MarqueeVisible
+              section={article}
+              className="marquee__visibleWrap"
+            />
             <div className="marquee__hiddenInfo">
               <p>{article.info}</p>
             </div>
