e808ca0176c82f0e25ffe117a51fc6481046ba96
isabelgmeg <71519606+isabelgmeg@users.noreply.github.com>
2021-10-14 12:10:27 +0200

    added more info to object

--- src/utilities.js
+++ src/utilities.js
@@ -18,8 +18,21 @@ const articles = [
     pics: [
       "https://www.museothyssen.org/sites/default/files/styles/media_gallery_thumbnail/public/imagen/2021-03/copa_imhoff.jpg?h=8ff814de",
       "https://arsmagazine.com/wp-content/uploads/2021/04/aguamanil-e1618310857423.jpg",
+      "https://i0.wp.com/elasombrario.publico.es/wp-content/uploads/sites/1/2021/04/Cornelius-Floris.jpeg?resize=399%2C600&ssl=1",
     ],
     info: "Como parte de la celebración en 2021 del centenario del barón Thyssen, el museo reúne en sus salas una selección de veinte obras pertenecientes a la colección de Hans Heinrich Thyssen-Bornemisza antes de que esta se dispersara entre sus herederos y de la instalación de la mayoría de sus pinturas en el palacio de Villahermosa, actual sede del museo.",
+  },
+  {
+    tittle: "Colección Permanente",
+    type: "Exposición Permanente",
+    date: "Permanente",
+    pics: [
+      "https://www.museothyssen.org/sites/default/files/styles/4x3_medium/public/imagen/obras/1978.92_mujer-bano.jpg?h=ec52094f",
+      "https://www.museothyssen.org/sites/default/files/styles/4x3_medium/public/imagen/obras/CTB.1984.8_mata-mua-erase-vez.jpg?h=d95180a3",
+      "https://www.museothyssen.org/sites/default/files/styles/4x3_medium/public/imagen/obras/1976.90_autorretrato-gorra-dos-cadenas.jpg?h=6429b14c",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYYyXrQmsablUVozpwZr_RoLbjosX_CmQZjQ&usqp=CAU",
+    ],
+    info: "Duccio, Van Eyck, Durero, Caravaggio, Rembrandt, Canaletto, Monet, Degas, Morisot, Van Gogh, Gauguin, Kirchner, Mondrian, O'Keeffe, Hopper…son solo algunos de los grandes nombres de la pintura cuya obra puede contemplarse en el museo. Cerca de mil obras que permiten recorrer la historia de la pintura occidental desde el siglo XIII hasta el XX.",
   },
 ];
 
