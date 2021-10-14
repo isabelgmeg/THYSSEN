0e92f14b19de19139e6155e6512bf29fbc512e6d
isabelgmeg <71519606+isabelgmeg@users.noreply.github.com>
2021-10-14 11:48:31 +0200

    passed info by props to component

--- src/components/Marquee/Marquee.jsx
+++ src/components/Marquee/Marquee.jsx
@@ -1,8 +1,6 @@
-import { useEffect } from "react";
-
 import "./Marquee.scss";
 
-export default function Marquee({ pics, tittle, text }) {
+export default function Marquee({ info }) {
   const populateImages = (images) => {
     let populatedImages = [...images];
     let i = 0;
@@ -13,27 +11,30 @@ export default function Marquee({ pics, tittle, text }) {
     return populatedImages;
   };
 
-  useEffect(() => {
-    populateImages(pics);
-  }, []);
-
   return (
-    <div className="marquee">
-      <div className="marquee__visibleWrap">
-        {populateImages(pics).map((singlePic, index) => {
-          return (
-            <div key={index} className="marquee__visibleWrap__element">
-              <div className="marquee__image__wrap">
-                <img src={singlePic} alt={tittle}></img>
-              </div>
-              <p>{tittle}</p>
+    <div className="displayArtciles">
+      {info.map((article, index) => {
+        console.log(article);
+        return (
+          <div key={index} className="marquee">
+            <div className="marquee__visibleWrap">
+              {populateImages(article.pics).map((singlePic, index) => {
+                return (
+                  <div key={index} className="marquee__visibleWrap__element">
+                    <div className="marquee__image__wrap">
+                      <img src={singlePic} alt={article.tittle}></img>
+                    </div>
+                    <p>{article.tittle}</p>
+                  </div>
+                );
+              })}
             </div>
-          );
-        })}
-      </div>
-      <div className="marquee__hiddenInfo">
-        <p>{text}</p>
-      </div>
+            <div className="marquee__hiddenInfo">
+              <p>{article.info}</p>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
---
+++ src/utilities.js
@@ -0,0 +1,26 @@
+const articles = [
+  {
+    tittle: "La maquina de Magritte",
+    type: "Exposición Temporal",
+    date: "14.09.21-10.01.22",
+    pics: [
+      "http://4.bp.blogspot.com/-52X2y4FUZkU/VCw-II9bO5I/AAAAAAAAga8/g1zWfgV2oCQ/s1600/carpaccio-joven-caballero-thyssen17-detalle-del-mismo.jpg",
+      "http://4.bp.blogspot.com/-52X2y4FUZkU/VCw-II9bO5I/AAAAAAAAga8/g1zWfgV2oCQ/s1600/carpaccio-joven-caballero-thyssen17-detalle-del-mismo.jpg",
+      "http://4.bp.blogspot.com/-52X2y4FUZkU/VCw-II9bO5I/AAAAAAAAga8/g1zWfgV2oCQ/s1600/carpaccio-joven-caballero-thyssen17-detalle-del-mismo.jpg",
+      "http://4.bp.blogspot.com/-52X2y4FUZkU/VCw-II9bO5I/AAAAAAAAga8/g1zWfgV2oCQ/s1600/carpaccio-joven-caballero-thyssen17-detalle-del-mismo.jpg",
+    ],
+    info: "El cuadro restaurado se presenta junto a un vídeo que explica e ilustra la intervención realizada y los resultados obtenidos gracias al estudio técnico que acompaña a toda restauración, recogidos también en una publicación monográfica.",
+  },
+  {
+    tittle: "Tesoros de la colección",
+    type: "Exposición Temporal",
+    date: "14.09.21-10.01.22",
+    pics: [
+      "https://www.museothyssen.org/sites/default/files/styles/media_gallery_thumbnail/public/imagen/2021-03/copa_imhoff.jpg?h=8ff814de",
+      "https://arsmagazine.com/wp-content/uploads/2021/04/aguamanil-e1618310857423.jpg",
+    ],
+    info: "Como parte de la celebración en 2021 del centenario del barón Thyssen, el museo reúne en sus salas una selección de veinte obras pertenecientes a la colección de Hans Heinrich Thyssen-Bornemisza antes de que esta se dispersara entre sus herederos y de la instalación de la mayoría de sus pinturas en el palacio de Villahermosa, actual sede del museo.",
+  },
+];
+
+export default articles;
